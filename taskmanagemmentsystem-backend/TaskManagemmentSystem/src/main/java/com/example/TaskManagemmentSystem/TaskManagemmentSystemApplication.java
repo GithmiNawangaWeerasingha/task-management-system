4897ad874db97257b675804319fe66bb316d53af
Githmi Nawanga <90828906+GithmiNawangaWeerasingha@users.noreply.github.com>
2024-01-24 19:16:04 +0530

    first commit

---
+++ taskmanagemmentsystem-backend/TaskManagemmentSystem/src/main/java/com/example/TaskManagemmentSystem/TaskManagemmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagemmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagemmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagemmentSystemApplication.class, args);
+	}
+
+}
---
+++ taskmanagemmentsystem-backend/TaskManagemmentSystem/src/test/java/com/example/TaskManagemmentSystem/TaskManagemmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagemmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagemmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
